413bcc5b498bd8fbded2b45763db383e55c81363
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-06-24 09:48:28 -0400

    remove individual subfolders

--- 01-basic/cypress/integration/answer.js
+++
@@ -1,6 +0,0 @@
-/// <reference types="cypress" />
-it('loads', () => {
-  cy.visit('localhost:3000')
-  // use "selector", "text" arguments to "cy.contains"
-  cy.contains('a', 'TodoMVC')
-})
--- 01-basic/cypress/integration/spec.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-it('loads', () => {
-  cy.visit('localhost:3000')
-  // this assertion fails on purpose
-  // can you fix it?
-  cy.contains('Part of TodoMVC')
-})
