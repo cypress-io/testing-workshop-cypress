fbe575d13dc4563a6c644a6acd9bf3fab4d78072
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-19 12:43:01 -0500

    11: retry

--- cypress/integration/11-retry-ability/spec.js
+++ cypress/integration/11-retry-ability/spec.js
@@ -1,78 +1,107 @@
 /// <reference types="cypress" />
-beforeEach(function resetData () {
-  cy.request('POST', '/reset', {
+beforeEach(function resetData() {
+  cy.request("POST", "/reset", {
     todos: []
-  })
-})
+  });
+});
 
-beforeEach(function visitSite () {
-  cy.visit('/')
-})
+beforeEach(function visitSite() {
+  cy.visit("/");
+});
 
-it('shows UL', function () {
-  cy.get('.new-todo')
-    .type('todo A{enter}')
-    .type('todo B{enter}')
-    .type('todo C{enter}')
-    .type('todo D{enter}')
-  cy.contains('ul', 'todo A')
-  // confirm that the above element
-  //  1. is visible
-  //  2. has class "todo-list"
-  //  3. css property "list-style-type" is equal "none"
-})
+it("shows UL", function() {
+  cy.get(".new-todo")
+    .type("todo A{enter}")
+    .type("todo B{enter}")
+    .type("todo C{enter}")
+    .type("todo D{enter}");
+  cy.contains("ul", "todo A")
+    // confirm that the above element
+    //  1. is visible
+    .should("be.visible")
+    //  2. has class "todo-list"
+    .and("have.class", "todo-list")
+    //  3. css property "list-style-type" is equal "none"
+    .and("have.css", "list-style-type", "none");
+});
 
-it('shows UL - TDD', function () {
-  cy.get('.new-todo')
-    .type('todo A{enter}')
-    .type('todo B{enter}')
-    .type('todo C{enter}')
-    .type('todo D{enter}')
-  cy.contains('ul', 'todo A').then($ul => {
+it("shows UL - TDD", function() {
+  cy.get(".new-todo")
+    .type("todo A{enter}")
+    .type("todo B{enter}")
+    .type("todo C{enter}")
+    .type("todo D{enter}");
+  cy.contains("ul", "todo A").then($ul => {
     // use TDD assertions
     // $ul is visible
+    assert.isTrue($ul.is(":visible"), "ul is visible");
     // $ul has class "todo-list"
+    assert.include($ul[0].className, "todo-list");
+    assert.isTrue($ul.hasClass("todo-list"));
     // $ul css has "list-style-type" = "none"
-  })
-})
+    assert.equal($ul.css("list-style-type"), "none");
+  });
+});
 
-it('every item starts with todo', function () {
-  cy.get('.new-todo')
-    .type('todo A{enter}')
-    .type('todo B{enter}')
-    .type('todo C{enter}')
-    .type('todo D{enter}')
-  cy.get('.todo label').should($labels => {
+it("every item starts with todo", function() {
+  cy.get(".new-todo")
+    .type("todo A{enter}")
+    .type("todo B{enter}")
+    .type("todo C{enter}")
+    .type("todo D{enter}");
+  cy.get(".todo label").should($labels => {
     // confirm that there are 4 labels
+    expect($labels).to.have.length(4);
     // and that each one starts with "todo-"
-  })
-})
+    $labels.each((k, el) => {
+      expect(el.textContent).to.match(/^todo /);
+    });
+  });
+});
 
-it('has the right label', () => {
-  cy.get('.new-todo').type('todo A{enter}')
+it("has the right label", () => {
+  cy.get(".new-todo").type("todo A{enter}");
   // ?
-})
+  cy.get(".todo-list li")
+    .find("label")
+    .should("contain", "todo A");
+});
 
-it('has two labels', () => {
-  cy.get('.new-todo').type('todo A{enter}')
+it("has two labels", () => {
+  cy.get(".new-todo").type("todo A{enter}");
   // ?
+  cy.get(".todo-list li")
+    .find("label")
+    .should("contain", "todo A");
+  cy.get(".new-todo").type("todo B{enter}");
+  // ?
+  cy.get(".todo-list li")
+    .find("label")
+    .should("contain", "todo B");
+});
 
-  cy.get('.new-todo').type('todo B{enter}')
+it("solution 1: merges queries", () => {
+  cy.get(".new-todo").type("todo A{enter}");
   // ?
-})
+  cy.get(".todo-list li label").should("contain", "todo A");
 
-it('solution 1: merges queries', () => {
-  cy.get('.new-todo').type('todo A{enter}')
+  cy.get(".new-todo").type("todo B{enter}");
   // ?
+  cy.get(".todo-list li label").should("contain", "todo B");
+});
 
-  cy.get('.new-todo').type('todo B{enter}')
+it("solution 2: alternate commands and assertions", () => {
+  cy.get(".new-todo").type("todo A{enter}");
   // ?
-})
+  cy.get(".todo-list li")
+    .should("have.length", 1)
+    .find("label")
+    .should("contain", "todo A");
 
-it('solution 2: alternate commands and assertions', () => {
-  cy.get('.new-todo').type('todo A{enter}')
+  cy.get(".new-todo").type("todo B{enter}");
   // ?
-
-  cy.get('.new-todo').type('todo B{enter}')
-  // ?
-})
+  cy.get(".todo-list li")
+    .should("have.length", 2)
+    .find("label")
+    .should("contain", "todo B");
+});
--- todomvc/app.js
+++ todomvc/app.js
@@ -1,19 +1,19 @@
 /* global Vue, Vuex, axios, FileReader, window, Promise */
 /* eslint-disable no-console */
-;(function () {
-  Vue.use(Vuex)
+(function() {
+  Vue.use(Vuex);
 
-  function randomId () {
+  function randomId() {
     return Math.random()
       .toString()
-      .substr(2, 10)
+      .substr(2, 10);
   }
 
   const store = new Vuex.Store({
     state: {
       loading: true,
       todos: [],
-      newTodo: ''
+      newTodo: ""
     },
     getters: {
       newTodo: state => state.newTodo,
@@ -21,45 +21,45 @@
       loading: state => state.loading
     },
     mutations: {
-      SET_LOADING (state, flag) {
-        state.loading = flag
+      SET_LOADING(state, flag) {
+        state.loading = flag;
       },
-      SET_TODOS (state, todos) {
-        state.todos = todos
+      SET_TODOS(state, todos) {
+        state.todos = todos;
       },
-      SET_NEW_TODO (state, todo) {
-        state.newTodo = todo
+      SET_NEW_TODO(state, todo) {
+        state.newTodo = todo;
       },
-      ADD_TODO (state, todoObject) {
-        console.log('add todo', todoObject)
-        state.todos.push(todoObject)
+      ADD_TODO(state, todoObject) {
+        console.log("add todo", todoObject);
+        state.todos.push(todoObject);
       },
-      REMOVE_TODO (state, todo) {
-        let todos = state.todos
-        todos.splice(todos.indexOf(todo), 1)
+      REMOVE_TODO(state, todo) {
+        let todos = state.todos;
+        todos.splice(todos.indexOf(todo), 1);
       },
-      CLEAR_NEW_TODO (state) {
-        state.newTodo = ''
-        console.log('clearing new todo')
+      CLEAR_NEW_TODO(state) {
+        state.newTodo = "";
+        console.log("clearing new todo");
       }
     },
     actions: {
-      loadTodos ({ commit }) {
-        commit('SET_LOADING', true)
+      loadTodos({ commit }) {
+        commit("SET_LOADING", true);
 
         axios
-          .get('/todos')
+          .get("/todos")
           .then(r => r.data)
           .then(todos => {
-            console.log('got %d todos', todos.length)
-            commit('SET_TODOS', todos)
-            commit('SET_LOADING', false)
+            console.log("got %d todos", todos.length);
+            commit("SET_TODOS", todos);
+            commit("SET_LOADING", false);
           })
           .catch(e => {
-            console.error('could not load todos')
-            console.error(e.message)
-            console.error(e.response.data)
-          })
+            console.error("could not load todos");
+            console.error(e.message);
+            console.error(e.response.data);
+          });
       },
 
       /**
@@ -68,48 +68,51 @@
        * @param {any} { commit }
        * @param {string} todo Message
        */
-      setNewTodo ({ commit }, todo) {
-        commit('SET_NEW_TODO', todo)
+      setNewTodo({ commit }, todo) {
+        commit("SET_NEW_TODO", todo);
       },
-      addTodo ({ commit, state }) {
+      addTodo({ commit, state }) {
         if (!state.newTodo) {
           // do not add empty todos
-          return
+          return;
         }
         const todo = {
           title: state.newTodo,
           completed: false,
           id: randomId()
-        }
-        axios.post('/todos', todo).then(() => {
-          commit('ADD_TODO', todo)
-        })
+        };
+        axios.post("/todos", todo).then(() => {
+          setTimeout(() => {
+            // add delay
+            commit("ADD_TODO", todo);
+          }, 52); // of 100ms
+        });
       },
-      removeTodo ({ commit }, todo) {
+      removeTodo({ commit }, todo) {
         axios.delete(`/todos/${todo.id}`).then(() => {
-          console.log('removed todo', todo.id, 'from the server')
-          commit('REMOVE_TODO', todo)
-        })
+          console.log("removed todo", todo.id, "from the server");
+          commit("REMOVE_TODO", todo);
+        });
       },
-      clearNewTodo ({ commit }) {
-        commit('CLEAR_NEW_TODO')
+      clearNewTodo({ commit }) {
+        commit("CLEAR_NEW_TODO");
       },
       // example promise-returning action
-      addTodoAfterDelay ({ commit }, { milliseconds, title }) {
+      addTodoAfterDelay({ commit }, { milliseconds, title }) {
         return new Promise(resolve => {
           setTimeout(() => {
             const todo = {
               title,
               completed: false,
               id: randomId()
-            }
-            commit('ADD_TODO', todo)
-            resolve()
-          }, milliseconds)
-        })
+            };
+            commit("ADD_TODO", todo);
+            resolve();
+          }, milliseconds);
+        });
       }
     }
-  })
+  });
 
   // app Vue instance
   const app = new Vue({
@@ -117,48 +120,48 @@
     data: {
       file: null
     },
-    el: '.todoapp',
+    el: ".todoapp",
 
-    created () {
-      this.$store.dispatch('loadTodos')
+    created() {
+      this.$store.dispatch("loadTodos");
     },
 
     // computed properties
     // https://vuejs.org/guide/computed.html
     computed: {
-      loading () {
-        return this.$store.getters.loading
+      loading() {
+        return this.$store.getters.loading;
       },
-      newTodo () {
-        return this.$store.getters.newTodo
+      newTodo() {
+        return this.$store.getters.newTodo;
       },
-      todos () {
-        return this.$store.getters.todos
+      todos() {
+        return this.$store.getters.todos;
       }
     },
 
     // methods that implement data logic.
     // note there's no DOM manipulation here at all.
     methods: {
-      setNewTodo (e) {
-        this.$store.dispatch('setNewTodo', e.target.value)
+      setNewTodo(e) {
+        this.$store.dispatch("setNewTodo", e.target.value);
       },
 
-      addTodo (e) {
-        e.target.value = ''
-        this.$store.dispatch('addTodo')
-        this.$store.dispatch('clearNewTodo')
+      addTodo(e) {
+        e.target.value = "";
+        this.$store.dispatch("addTodo");
+        this.$store.dispatch("clearNewTodo");
       },
 
-      removeTodo (todo) {
-        this.$store.dispatch('removeTodo', todo)
+      removeTodo(todo) {
+        this.$store.dispatch("removeTodo", todo);
       }
     }
-  })
+  });
 
   // if you want to expose "app" globally only
   // during end-to-end tests you can guard it using "window.Cypress" flag
   // if (window.Cypress) {
-  window.app = app
+  window.app = app;
   // }
-})()
+})();
