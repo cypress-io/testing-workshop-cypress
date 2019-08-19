0c77aeaf0f587c0b6b27269530816523def7346f
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-19 09:04:26 -0500

    04: reset state

--- cypress/integration/04-reset-state/spec.js
+++ cypress/integration/04-reset-state/spec.js
@@ -2,20 +2,23 @@
 beforeEach(() => {
   // application should be running at port 3000
   // and the "localhost:3000" is set as "baseUrl" in "cypress.json"
-  cy.visit('/')
-})
-it('loads', () => {
-  cy.contains('h1', 'todos')
-})
+  cy.request("POST", "/reset", {
+    todos: []
+  });
+  cy.visit("/");
+});
+it("loads", () => {
+  cy.contains("h1", "todos");
+});
 /**
  * Adds a todo item
  * @param {string} text
  */
 const addItem = text => {
-  cy.get('.new-todo').type(`${text}{enter}`)
-}
-it('adds two items', () => {
-  addItem('first item')
-  addItem('second item')
-  cy.get('li.todo').should('have.length', 2)
-})
+  cy.get(".new-todo").type(`${text}{enter}`);
+};
+it("adds two items", () => {
+  addItem("first item");
+  addItem("second item");
+  cy.get("li.todo").should("have.length", 2);
+});
