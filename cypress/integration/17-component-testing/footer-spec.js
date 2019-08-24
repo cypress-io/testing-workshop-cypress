fc954db38966c0e01f719185799f5ef2e12bf96b
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-24 09:58:28 -0500

    testing

--- cypress/integration/17-component-testing/footer-spec.js
+++ cypress/integration/17-component-testing/footer-spec.js
@@ -1,27 +1,30 @@
 /// <reference types="cypress" />
 // uncomment these as needed
-// import React from 'react'
-// import Footer from './Footer'
+import React from "react";
+// import Footer from "./Footer";
 // adds custom command "cy.mount"
-import 'cypress-react-unit-test'
-// import { filters } from './filters'
+import "cypress-react-unit-test";
+import { filters } from "./filters";
 
 beforeEach(() => {
-  cy.viewport(500, 300)
-})
-it('shows Footer', () => {
+  cy.viewport(500, 300);
+});
+it("shows Footer", () => {
   // mount imported Footer component
   // make sure to pass "count={10}"
   // and "nowShowing={filters.ALL_TODOS}" props
+  // cy.mount(<Footer count={10} nowShowing={filters.ALL_TODOS} />);
+
   //
   // get "footer" and make assertions
   //  - it should be visible
   //  - it should have "show-all" link with class "selected"
-})
+  cy.get("footer").should("be.visible");
+});
 
-it('clears completed on click', () => {
+it("clears completed on click", () => {
   // mounts imported Footer component
   // with onClearCompleted being cy.stub()
   // finds "button class=clear-completed" and clicks
   // and then checks that our stub was called once
-})
+});
