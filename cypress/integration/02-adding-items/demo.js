cf23e8ded5814d260e834201c15c36d8f0a949b8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-11-25 21:51:08 -0500

    try resource observer - but it only see completed requests

--- cypress/integration/02-adding-items/demo.js
+++ cypress/integration/02-adding-items/demo.js
@@ -12,15 +12,38 @@ if (isLocalHost()) {
 
 beforeEach(function visitSite () {
   cy.log('Visiting', Cypress.config('baseUrl'))
-  cy.visit('/')
+
+  cy.server()
+  cy.route('/todos').as('loading')
+  cy.visit('/', {
+    onBeforeLoad (win) {
+      function perf_observer (list, observer) {
+        console.log('list', list.getEntries())
+      }
+      var observer2 = new win.PerformanceObserver(perf_observer)
+      observer2.observe({ entryTypes: ['resource'] })
+    }
+  })
+  cy.window()
+    .its('performance')
+    .invoke('getEntries')
+    .its('length')
+    .then(console.log)
+
+  cy.wait('@loading')
+  cy.window()
+    .its('performance')
+    .invoke('getEntries')
+    .its('length')
+    .then(console.log)
 })
 
 it('adds items', function () {
-  cy.get('.new-todo')
-    .type('todo A{enter}')
-    .type('todo B{enter}')
-    .type('todo C{enter}')
-    .type('todo D{enter}')
-  cy.get('.todo-list li') // command
-    .should('have.length', 4) // assertion
+  // cy.get('.new-todo')
+  //   .type('todo A{enter}')
+  //   .type('todo B{enter}')
+  //   .type('todo C{enter}')
+  //   .type('todo D{enter}')
+  // cy.get('.todo-list li') // command
+  //   .should('have.length', 4) // assertion
 })
