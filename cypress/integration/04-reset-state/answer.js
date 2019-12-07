71e3bfceaa7dc7c0a79494777deb118ff03113e3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-12-07 16:04:47 -0500

    adding resetting data using cy.task

--- cypress/integration/04-reset-state/answer.js
+++ cypress/integration/04-reset-state/answer.js
@@ -1,21 +1,64 @@
 /// <reference types="cypress" />
-beforeEach(() => {
-  cy.request('POST', '/reset', {
-    todos: []
+describe('reset data using XHR call', () => {
+  beforeEach(() => {
+    cy.request('POST', '/reset', {
+      todos: []
+    })
+  })
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  /**
+   * Adds a todo item
+   * @param {string} text
+   */
+  const addItem = text => {
+    cy.get('.new-todo').type(`${text}{enter}`)
+  }
+  it('adds two items', () => {
+    addItem('first item')
+    addItem('second item')
+    cy.get('li.todo').should('have.length', 2)
   })
 })
-beforeEach(() => {
-  cy.visit('/')
-})
-/**
- * Adds a todo item
- * @param {string} text
- */
-const addItem = text => {
-  cy.get('.new-todo').type(`${text}{enter}`)
-}
-it('adds two items', () => {
-  addItem('first item')
-  addItem('second item')
-  cy.get('li.todo').should('have.length', 2)
+
+describe('reset data using a task', () => {
+  beforeEach(() => {
+    cy.task('resetData')
+  })
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  /**
+   * Adds a todo item
+   * @param {string} text
+   */
+  const addItem = text => {
+    cy.get('.new-todo').type(`${text}{enter}`)
+  }
+  it('adds two items', () => {
+    addItem('first item')
+    addItem('second item')
+    cy.get('li.todo').should('have.length', 2)
+  })
+
+  it('sets data to complex object right away', () => {
+    cy.task('resetData', {
+      todos: [{
+        id: '123456abc',
+        completed: true,
+        title: 'reset data before test'
+      }]
+    })
+
+    cy.visit('/')
+  })
+
+  it('sets data using fixture', () => {
+    cy.fixture('two-items').then(todos => {
+      cy.task('resetData', { todos })
+    })
+
+    cy.visit('/')
+  })
 })
--- cypress/integration/04-reset-state/spec.js
+++ cypress/integration/04-reset-state/spec.js
@@ -1,18 +1,20 @@
 /// <reference types="cypress" />
-beforeEach(() => {
-  // application should be running at port 3000
-  // and the "localhost:3000" is set as "baseUrl" in "cypress.json"
-  cy.visit('/')
-})
-/**
- * Adds a todo item
- * @param {string} text
- */
-const addItem = text => {
-  cy.get('.new-todo').type(`${text}{enter}`)
-}
-it('adds two items', () => {
-  addItem('first item')
-  addItem('second item')
-  cy.get('li.todo').should('have.length', 2)
+describe('reset data using XHR call', () => {
+  beforeEach(() => {
+    // application should be running at port 3000
+    // and the "localhost:3000" is set as "baseUrl" in "cypress.json"
+    cy.visit('/')
+  })
+  /**
+   * Adds a todo item
+   * @param {string} text
+   */
+  const addItem = text => {
+    cy.get('.new-todo').type(`${text}{enter}`)
+  }
+  it('adds two items', () => {
+    addItem('first item')
+    addItem('second item')
+    cy.get('li.todo').should('have.length', 2)
+  })
 })
