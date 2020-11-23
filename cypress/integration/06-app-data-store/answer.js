188d8d2416aca3f674c6c6a1e8c374b9531bf0f4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-11-23 15:06:16 -0500

    use full test title in the screenshot

--- cypress/integration/06-app-data-store/answer.js
+++ cypress/integration/06-app-data-store/answer.js
@@ -1,27 +1,4 @@
 /// <reference types="cypress" />
-beforeEach(() => {
-  cy.request('POST', '/reset', {
-    todos: []
-  })
-})
-beforeEach(() => {
-  cy.visit('/')
-})
-beforeEach(function stubRandomId() {
-  let count = 1
-  cy.window()
-    .its('Math')
-    .then(Math => {
-      cy.stub(Math, 'random', () => {
-        return `0.${count++}`
-      }).as('random') // save reference to the spy
-    })
-})
-afterEach(() => {
-  // makes debugging failing tests much simpler
-  cy.screenshot('runner')
-})
-
 /**
  * Adds a todo item
  * @param {string} text
@@ -30,80 +7,105 @@ const addItem = text => {
   cy.get('.new-todo').type(`${text}{enter}`)
 }
 
-it('adds items to store', () => {
-  addItem('something')
-  addItem('something else')
-  cy.window()
-    .its('app.$store.state.todos')
-    .should('have.length', 2)
-})
-
-it('creates an item with id 1', () => {
-  cy.server()
-  cy.route('POST', '/todos').as('new-item')
-  addItem('something')
-  cy.wait('@new-item')
-    .its('request.body')
-    .should('deep.equal', {
-      id: '1',
-      title: 'something',
-      completed: false
+describe('App Data Store', () => {
+  beforeEach(() => {
+    cy.request('POST', '/reset', {
+      todos: []
     })
-})
+  })
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  beforeEach(function stubRandomId() {
+    let count = 1
+    cy.window()
+      .its('Math')
+      .then(Math => {
+        cy.stub(Math, 'random', () => {
+          return `0.${count++}`
+        }).as('random') // save reference to the spy
+      })
+  })
+  afterEach(function () {
+    // makes debugging failing tests much simpler
+    cy.screenshot(this.currentTest.fullTitle())
+  })
 
-it('calls spy twice', () => {
-  addItem('something')
-  addItem('else')
-  cy.get('@random').should('have.been.calledTwice')
-})
+  it('adds items to store', () => {
+    addItem('something')
+    addItem('something else')
+    cy.window()
+      .its('app.$store.state.todos')
+      .should('have.length', 2)
+  })
 
-it('puts todos in the store', () => {
-  addItem('something')
-  addItem('else')
-  cy.window()
-    .its('app.$store.state.todos')
-    .should('deep.equal', [
-      { title: 'something', completed: false, id: '1' },
-      { title: 'else', completed: false, id: '2' }
-    ])
-})
+  it('creates an item with id 1', () => {
+    cy.server()
+    cy.route('POST', '/todos').as('new-item')
+    addItem('something')
+    cy.wait('@new-item')
+      .its('request.body')
+      .should('deep.equal', {
+        id: '1',
+        title: 'something',
+        completed: false
+      })
+  })
 
-it('adds todos via app', () => {
-  // bypass the UI and call app's actions directly from the test
-  // app.$store.dispatch('setNewTodo', <desired text>)
-  // app.$store.dispatch('addTodo')
-  // using https://on.cypress.io/invoke
-  // bypass the UI and call app's actions directly from the test
-  // app.$store.dispatch('setNewTodo', <desired text>)
-  // app.$store.dispatch('addTodo')
-  cy.window()
-    .its('app.$store')
-    .invoke('dispatch', 'setNewTodo', 'new todo')
+  it('calls spy twice', () => {
+    addItem('something')
+    addItem('else')
+    cy.get('@random').should('have.been.calledTwice')
+  })
 
-  cy.window()
-    .its('app.$store')
-    .invoke('dispatch', 'addTodo')
-  // and then check the UI
-  cy.contains('li.todo', 'new todo')
-})
+  it('puts todos in the store', () => {
+    addItem('something')
+    addItem('else')
+    cy.window()
+      .its('app.$store.state.todos')
+      .should('deep.equal', [
+        { title: 'something', completed: false, id: '1' },
+        { title: 'else', completed: false, id: '2' }
+      ])
+  })
 
-it('handles todos with blank title', { retries: 4 }, () => {
-  // bypass the UI and call app's actions directly from the test
-  // app.$store.dispatch('setNewTodo', <desired text>)
-  // app.$store.dispatch('addTodo')
-  cy.window()
-    .its('app.$store')
-    .invoke('dispatch', 'setNewTodo', '  ')
+  it('adds todos via app', () => {
+    // bypass the UI and call app's actions directly from the test
+    // app.$store.dispatch('setNewTodo', <desired text>)
+    // app.$store.dispatch('addTodo')
+    // using https://on.cypress.io/invoke
+    // bypass the UI and call app's actions directly from the test
+    // app.$store.dispatch('setNewTodo', <desired text>)
+    // app.$store.dispatch('addTodo')
+    cy.window()
+      .its('app.$store')
+      .invoke('dispatch', 'setNewTodo', 'new todo')
 
-  cy.window()
-    .its('app.$store')
-    .invoke('dispatch', 'addTodo')
+    cy.window()
+      .its('app.$store')
+      .invoke('dispatch', 'addTodo')
+    // and then check the UI
+    cy.contains('li.todo', 'new todo')
+  })
 
-  // confirm the application is not breaking
-  cy.get('li.todo')
-    .should('have.length', 1)
-    .first()
-    .should('not.have.class', 'completed')
-    .find('label')
-    .should('have.text', '  ')
+  it('handles todos with blank title', { retries: 2 }, () => {
+    // bypass the UI and call app's actions directly from the test
+    // app.$store.dispatch('setNewTodo', <desired text>)
+    // app.$store.dispatch('addTodo')
+    cy.window()
+      .its('app.$store')
+      .invoke('dispatch', 'setNewTodo', '  ')
+
+    cy.window()
+      .its('app.$store')
+      .invoke('dispatch', 'addTodo')
+
+    // confirm the application is not breaking
+    cy.get('li.todo')
+      .should('have.length', 1)
+      .first()
+      .should('not.have.class', 'completed')
+      .find('label')
+      .should('have.text', '  ')
+  })
 })
